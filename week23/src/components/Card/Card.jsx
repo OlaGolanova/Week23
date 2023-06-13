313c993423658efec1c467905ad77ce1bc500a92
Ola <musicola@mail.ru>
2023-06-13 12:50:58 +0300

    add scss rules

---
+++ week23/src/components/Card/Card.jsx
@@ -0,0 +1,32 @@
+
+import './Card.scss';
+
+
+
+function Card(props) {
+    return (
+        <div className="card">
+
+            <div className={props.classNameColorTariff} >
+                <div className= "card__tariff">{props.tariff}</div>
+            </div>
+            <div className= {props.classNameColorPrice}  >
+                <div className="card__price">
+                    <div className="card__price-rub" >руб</div>
+                    <div className="card__price-fix"> {props.price}</div>
+                    <div className="card__price-month">/мес</div>
+                </div>
+            </div>
+
+            <div className="card__speed">
+                {props.speed}
+            </div>
+
+            <div  className="card__descr">
+                {props.descr}
+            </div>
+        </div>
+    );
+}
+
+export default Card;
